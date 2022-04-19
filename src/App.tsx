30f475b98009a57d729c697b7eb58428220cdbe6
Stefan Bollmann <stefan.bollmann@rwth-aachen.de>
2022-04-19 14:10:01 +0200

    (Too) Simple useEffect usage in App.tsx with simple log output.

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,13 @@ import './App.css';
 import Title from "./component/Title";
 import {exportedDatas} from "./repo/Datas";
 import Gallery from "./component/Gallery";
+import {useEffect} from "react";
 
 export default function App() {
+
+    useEffect(() =>
+    console.log("useEffect used.")
+    )
 
   return (
     <div>
