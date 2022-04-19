9f554f82cb25b5e647c9ff08906ed6ddcc435b66
Stefan Bollmann <stefan.bollmann@rwth-aachen.de>
2022-04-19 10:05:50 +0200

    Button now in gallery. Todo: Centered

--- src/component/Gallery.tsx
+++ src/component/Gallery.tsx
@@ -1,12 +1,24 @@
 import {Character} from "../model/Character";
 import CharacterCard from "./CharacterCard";
 import "./Gallery.css"
+import React, {useState} from "react";
 
 type characterGalleryProps = {
     characters : Character[]
 }
 
 export default function Gallery({characters}:characterGalleryProps){
-    return <div id="gallery"> {characters.map( obj => <CharacterCard character={obj} />)} </div>
+    const [count, setCount] = useState<number>(0)
+
+    console.log(count)
+
+    const onButtonClick = () => {
+        console.log("Click me!")
+        setCount(count+1)
+
+    }
+    return <div id="gallery"><div>Button clicked: {count}</div>
+    <button onClick={onButtonClick} id="gallery-button">Click me!</button>
+        <div id="gallery-cards"> {characters.map( obj => <CharacterCard character={obj} />)} </div></div>
 }
 
