632cd36b4859427711819bd7c91e08be5d4527d4
Stefan Bollmann <stefan.bollmann@rwth-aachen.de>
2022-04-19 09:59:04 +0200

    Button on main page to count up.

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import Title from "./component/Title";
 import {exportedDatas} from "./repo/Datas";
 import Gallery from "./component/Gallery";
 
 export default function App() {
+    const [count, setCount] = useState<number>(0)
+
+    console.log(count)
+
+    const onButtonClick = () => {
+        console.log("Click me!")
+        setCount(count+1)
+
+    }
   return (
     <div>
 
       <Title />
         <Gallery characters = {exportedDatas} />
-
+        <div>Button clicked: {count}</div>
+    <button onClick={onButtonClick}>Click me!</button>
     </div>
-  );
+  )
 }
 
