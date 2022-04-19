12a2ae96fa338cdfbc0217eda95ed85af3fdc528
Stefan Bollmann <stefan.bollmann@rwth-aachen.de>
2022-04-19 14:26:38 +0200

    UseEffect now at every buttonClick (dependent on count variable).

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,27 @@ import './App.css';
 import Title from "./component/Title";
 import {exportedDatas} from "./repo/Datas";
 import Gallery from "./component/Gallery";
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 
 export default function App() {
 
-    useEffect(() =>
-    console.log("useEffect used.")
+    const [count, setCount] = useState<number>(0)
+
+    useEffect(() => {
+        console.log("useEffect used. " + count)
+    }, [count]
     )
+
+    const onClick = () => {
+        setCount(count+1)
+    }
 
   return (
     <div>
 
       <Title />
         <Gallery characters = {exportedDatas} />
-
+<button onClick={onClick}>Click me.</button>
     </div>
   )
 }
