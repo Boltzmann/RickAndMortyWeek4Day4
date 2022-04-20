08b460a8819e2c0ea21f6239cbbc3f83a99a908b
Stefan Bollmann <stefan.bollmann@rwth-aachen.de>
2022-04-20 16:04:52 +0200

    Characters are shown with fetch in CharacterDetailsPage.

--- src/pages/CharacterDetailsPage.tsx
+++ src/pages/CharacterDetailsPage.tsx
@@ -1,8 +1,36 @@
+import React, {useEffect, useState} from "react";
+import {Character} from "../model/Character";
+import {useParams} from "react-router-dom";
+import CharacterCard from "../component/CharacterCard";
 
 
 export default function CharacterDetailsPage() {
+    const params = useParams()
+    const id = params.id
+    const [fetchedCharacter, setFetchedCharacter] = useState<Character>();
+    const url:string = "https://rickandmortyapi.com/api/character/" + id
+
+    const fetchCharacter = () => {
+        return fetch(url)
+            .then(response => {
+                if (response.ok) {
+                    return response.json()
+                }
+                throw new Error("Network error")
+            }).catch(console.error)
+    }
+
+    useEffect(() => {
+        fetchCharacter()
+            .then(body => {
+                setFetchedCharacter(body)
+            })
+    }, [])
 
     return (
-        <div>Character </div>
-    )
+        <div>
+            {fetchedCharacter && <CharacterCard character={fetchedCharacter}/>}
+        </div>
+    );
+
 }
