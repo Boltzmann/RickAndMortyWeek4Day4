6acdeae0a8caf2c8b3ca5cfd8ff5df0131cf1ae3
Stefan Bollmann <stefan.bollmann@rwth-aachen.de>
2022-04-20 12:28:46 +0200

    Links for different contents now centered below title in header. Used different flex boxes for this.

--- src/component/Title.tsx
+++ src/component/Title.tsx
@@ -4,11 +4,18 @@ import {Link} from "react-router-dom";
 export default function Title() {
     return (
         <header>
-            <img alt="Rick and Morty Icon" src="https://rickandmortyapi.com/icons/icon-512x512.png" />
-            <h1>Rick and Morty</h1>
+            <div id={"own-container"}>
+                <img alt="Rick and Morty Icon" src="https://rickandmortyapi.com/icons/icon-512x512.png" />
+                <h1>Rick and Morty</h1>
+            </div>
+            <p>
+            <Link to={"/home"}>
+                Home
+            </Link>
             <Link to={"/gallery"}>
                 Gallery
             </Link>
+            </p>
             </header>
     )
 }
